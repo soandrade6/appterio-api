5cc4ed0c00e3f08e71fbdc4747667aad859fe02a
CatalinaRpoS <catrestreposa@unal.edu.co>
2025-03-01 12:23:09 -0500

    feat: message on get route /

--- app/config.py
+++ app/config.py
@@ -5,5 +5,5 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
-SECRET_KEY = os.getenv("SECRET_KEY")
-DEBUG = os.getenv("DEBUG", "False") == "True"
+# SECRET_KEY = os.getenv("SECRET_KEY")
+# DEBUG = os.getenv("DEBUG", "False") == "True"
--- app/database.py
+++ app/database.py
@@ -16,3 +16,7 @@ def get_db():
         yield db
     finally:
         db.close()
+
+@app.get("/")
+async def root():
+    return {"message": "Bienvenido a AppTerio API"}
